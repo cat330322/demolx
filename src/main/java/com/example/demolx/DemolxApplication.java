367b19a23e3203da6ce17f850c5380794f25377a
cat330322 <345432510@qq.com>
2022-04-02 17:51:54 +0800

    20220402

---
+++ src/main/java/com/example/demolx/DemolxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demolx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemolxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demolx/DemolxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demolx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
